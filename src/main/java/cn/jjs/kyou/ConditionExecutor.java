d5c4bbe20f58369413ebdd563782dacf2bb5b33e
jiangjunshen <jiangjunshen@cai-inc.com>
2018-11-03 16:34:31 +0800

    添加一个简化if语句的工具类

---
+++ src/main/java/cn/jjs/kyou/ConditionExecutor.java
@@ -0,0 +1,34 @@
+package cn.jjs.kyou;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @author jiangjunshen
+ * @date 下午9:20 2018/10/30
+ */
+public class ConditionExecutor {
+
+    public static <T> void ifThen(Predicate<T> predicate, Consumer<T> consumer, T t) {
+        if (predicate.test(t)) {
+            consumer.accept(t);
+        }
+    }
+
+    public static <T, R> R ifThenElse(Predicate<T> predicate, Function<T, R> ifFunc, Function<T, R> elseFunc, T t) {
+        if (predicate.test(t)) {
+            return ifFunc.apply(t);
+        } else {
+            return elseFunc.apply(t);
+        }
+    }
+
+    public static <T, R> R ifThenElseDefault(Predicate<T> predicate, Function<T, R> ifFunc, T t, R r) {
+        if (predicate.test(t)) {
+            return ifFunc.apply(t);
+        } else {
+            return r;
+        }
+    }
+}
